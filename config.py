6c104acee9d1747afeca3fdb242a492cf7e70382
Strelizia-007 <143823798+Strelizia-007@users.noreply.github.com>
2023-11-05 09:14:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ API_HASH = getenv("API_HASH", "fabd9f89368de7cc31357522a0089a56")
 BOT_TOKEN = getenv("BOT_TOKEN", "6672385958:AAHhRjtPPxQc9ZwCZDf8enRNnOnnUZ4Ek1k")
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Bikash:Bikashop@bikash.cbkkx4c.mongodb.net/?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://ironman:<password>@cluster0.o1yjwis.mongodb.net/?retryWrites=true&w=majority")
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 62949449860))
 
